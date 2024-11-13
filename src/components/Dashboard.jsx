2fc2e0eb99d90d04a1eb23bfb8f18c6f35281c1c
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2024-11-13 20:55:10 +0900

    style 변경

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,19 +4,33 @@ import styled from "styled-components";
 const DashboardBox = styled.ul`
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
+  gap: 20px;
   width: 1280px;
   margin: 0 auto;
   padding: 100px 0;
 `;
 
 const SelectedPokemon = styled.li`
-  max-width: 100px;
   border: 2px dashed #ddd;
-  padding: 30px;
+  flex: 1;
+  padding: 30px 0;
   border-radius: 20px;
+  text-align: center;
 
   img {
     max-width: 100%;
+    width: 96px;
+  }
+
+  button {
+    display: block;
+    width: 50%;
+    margin: 10px auto;
+    border: 1px solid #ccc;
+    background: #fff;
+    position: relative;
+    z-index: 99;
   }
 `;
 
@@ -29,8 +43,9 @@ const Dashboard = ({ selectedPokemon, deletePokemon }) => {
         return (
           <SelectedPokemon key={index}>
             <img src={pokemon.img_url} alt={pokemon.korean_name} />
-            <p>{pokemon.korean_name}</p>
-            <p>No. {pokemon.id}</p>
+            <p>
+              No. {pokemon.id} {pokemon.korean_name}
+            </p>
             <button onClick={() => deletePokemon(pokemon.id)}>삭제</button>
           </SelectedPokemon>
         );
--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -47,7 +47,7 @@ const PokemonDetail = () => {
       <GlobalStyle />
       <DetailContainer>
         <DetailImg src={pokemon?.img_url} alt={pokemon?.korean_name} />
-        <h2>{pokemon?.korean_name}</h2>
+        <p>{pokemon?.korean_name}</p>
         <p>{pokemon?.description}</p>
         <p>타입 : {pokemon?.types.join(", ")}</p>
         <DetailButton onClick={handleBackButton}>뒤로가기</DetailButton>
